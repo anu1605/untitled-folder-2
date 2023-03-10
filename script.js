db745ff82ee2ac112a3b0e1c9aba17de2852a3cb
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-03-10 17:33:16 +0530

    form using jquery

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+
 
   var emptyInput;
   var tableCount = 0;
@@ -8,40 +8,42 @@
   var subjectList = [];
   var selectedSbject = [];
   var selectedHobbies = [];
-  var year;
-  var marks;
+  var response;
+  
+  
+
 
   
-  $("#submit").click(func);
+  $.ajaxSetup({
+    cache: false
+  });
+  $('.add').click(addFunc);
   
-  $(".subject_input > input").change(checkSubject);
+  
 
-  
-  function checkSubject() {
-    if (this.checked == true) {
-      document.getElementById("subect_error").innerHTML = "";
-      selectedSbject.push(this.value);
-    } else selectedSbject.delete(this.value);
-    console.log(selectedSbject);
-  }
   
   $("option").click(function(){
     { 
     if (this.selected) {
       selectedHobbies.push(this.value);
-      $("#hobbies_error").innerHTML = "";
+      $("#hobbies_error")[0].innerHTML = "";
     }
+    else selectedHobbies.splice(this.value.indexOf(),1);
   }
+
   });
   
+  $(document).ready(function(){
+
+    $("#submit").click(func);
   
+  });
   function func() {
     if (emptyInput != undefined) emptyInput.classList.remove("redBorder");
     // validate firstname
-    var firstName = document.getElementById("fname");
-    if (firstName.value == "") {
+    if ($("#fname")[0].value == "") {
       if (emptyInput != undefined) emptyInput.classList.remove("redBorder");
-      emptyInput = firstName;
+      emptyInput = $("#fname")[0];
       emptyInput.classList.add("redBorder");
       emptyInput.select();
       emptyInput.scrollIntoView();
@@ -49,10 +51,9 @@
     }
   
     // validate lastname
-    var lastName = document.getElementById("lname");
-    if (lastName.value == "") {
+    if ($("#lname")[0].value == "") {
       removeBorder();
-      emptyInput = lastName;
+      emptyInput = $("#lname")[0];
       emptyInput.classList.add("redBorder");
       emptyInput.select();
       emptyInput.scrollIntoView();
@@ -60,25 +61,30 @@
     }
   
     // validate email
-    var email = document.getElementById("email");
   
     if (!validateEmail()) {
-      removeBorder();
-      emptyInput = email;
+      if(emptyInput!=undefined && emptyInput != $("#error")[0]){
+        removeBorder();
+      }
+      emptyInput = $("#email")[0];
       emptyInput.classList.add("redBorder");
+      if(emptyInput!='')
+      // $('#error').innerHTML = "Enter valid Email";
       email.scrollIntoView();
       return;
     }
   
     // check if gender is selected
-  
-    if (  $("#male")[0].checked == false && $("#female")[0].checked == false) {
+
+    
+    if ( $("#male").is(':checked') == false && $("#female").is(':checked') == false) {
       removeBorder();
       emptyInput = $("#male")[0];
       emptyInput.classList.add("redBorder");
       emptyInput.scrollIntoView();
       return;
     }
+    
     // check if hobbies field is empty
 
   
@@ -86,68 +92,70 @@
       removeBorder();
       emptyInput = $("#hobbies")[0];
       emptyInput.classList.add("redBorder");
-      emptyInput.scrollIntoView;
+      emptyInput.scrollIntoView();
   
-      $("#hobbies_error").innerHTML = "Select Hobbies";
+      $("#hobbies_error")[0].innerHTML = "Select Hobbies";
       return;
-    } else $("#hobbies_error").innerHTML = "";
+    } else $("#hobbies_error")[0].innerHTML = "";
   
     // check if subject field is empty
   
     if (selectedSbject.length == 0) {
       if (!alreadyClick) addClass();
-      $("#option_container").scrollIntoView;
-      $("#subect_error").innerHTML = "Select Subject";
+      $("#option_container")[0].scrollIntoView();
+      $("#subect_error")[0].innerHTML = "Select Subject";
       return;
-    } else $("#subect_error").innerHTML = "";
+    } else $("#subect_error")[0].innerHTML = "";
   
     
     // validate table
     for (var i = 0; i <= tableCount; i++) {
-      if(document.getElementById("table_body").rows[i] != undefined)
-      if (!checkEmptyCell(4, document.getElementById("table_body").rows[i]))
+      if($("#table_body")[0].rows[i] != undefined)
+      if (!checkEmptyCell(4, $("#table_body")[0].rows[i]))
         return;
     }
   
     // image validation
     if (!fileValidation()) {
       return;
-    } else document.getElementById("image_error").innerHTML = "";
-    var passwordError = document.getElementById("pwd_message");
-    var password = document.getElementById("pwd");
-    if (password.value == "") {
+    } else $("#image_error")[0].innerHTML = "";
+
+    // password validation
+    if ($("#pwd")[0].value == "") {
       removeBorder();
-      emptyInput = password;
+      emptyInput = $("#pwd")[0];
       emptyInput.select();
       emptyInput.scrollIntoView();
       emptyInput.classList.add("redBorder");
-      passwordError.innerHTML = "enter password";
+      $("#pwd_message")[0].innerHTML = "enter password";
       return;
-    } else if (document.getElementsByClassName("invalid").length != 0) {
-      passwordError.innerHTML = "enter valid password";
+    } else if ($(".invalid").length != 0) {
+      $("#pwd_message")[0].innerHTML = "enter valid password";
       return;
     }
-    var confirm_password = document.getElementById("confirm_pwd");
-    if (confirm_password.value == "") {
+
+
+    
+    if ($("#confirm_pwd")[0].value == "") {
       removeBorder();
-      emptyInput = confirm_password;
+      emptyInput = $("#confirm_pwd")[0];
       emptyInput.select();
       emptyInput.scrollIntoView();
       emptyInput.classList.add("redBorder");
-      passwordError.innerHTML = "confirm password";
+      $("#pwd_message")[0].innerHTML = "confirm password";
       return;
     } else if (!confirmPassword()) {
-      passwordError.innerHTML = "confirm password is wrong";
+      $("#pwd_message")[0].innerHTML = "confirm password is wrong";
       return;
-    } else if (confirm_password.value.length != password.value.length) {
+    } else if ($("#confirm_pwd")[0].value.length != $("#pwd")[0].value.length) {
       removeBorder();
-      emptyInput = confirm_password;
+      emptyInput = $("#confirm_pwd")[0];
       emptyInput.select();
       emptyInput.scrollIntoView();
       emptyInput.classList.add("redBorder");
-      passwordError.innerHTML = "confirm password is wrong";
+      $("#pwd_message")[0].innerHTML = "confirm password is wrong";
       return;
-    } else passwordError.innerHTML = "";
+    } else $("#pwd_message")[0].innerHTML = "";
   
     // date validation
     if (!validDate()) {
@@ -155,14 +163,14 @@
     }
   
     // print output
-    var printContainer = document.getElementById("print");
+    var printContainer = $("#print")[0];
     printContainer.innerHTML = "";
-    var form = document.getElementById("information");
+    var form = $("#information")[0];
     var formData = new FormData(form);
   
     for (item of formData) {
       if (item[0] == "filename") {
-        var fileInput = document.getElementById("myFile");
+        var fileInput = $("#myFile");
         if (fileInput.files && fileInput.files[0]) {
           var reader = new FileReader();
           reader.onload = function (e) {
@@ -177,52 +185,68 @@
     }
   }
   
-  var male = document.getElementById("male");
-  male.addEventListener("click", genderValue);
-  var female = document.getElementById("female");
-  female.addEventListener("click", genderValue);
+
+  var menu_btn = $("#menu")[0];
   
-  function genderValue() {
-    if (male.checked == true || female.checked == true) {
-      male.value = "male";
-      female.value = "female";
-    }
-  }
-  
-  var menu_btn = document.getElementById("menu");
-  
-  menu_btn.addEventListener("click", addClass);
+  $("#menu").click(addClass);
   
   function addClass() {
-    var container = document.getElementById("option_container");
-    var arrow = document.getElementById("menu");
-    if (!alreadyClick) {
-      container.classList.add("expandible_class");
-      arrow.classList.remove("rotate_down");
-      arrow.classList.add("rotate_up");
+    if(!alreadyClick){
+      $("#menu")[0].classList.remove("rotate_down");
+      $("#menu")[0].classList.add("rotate_up");
+      $('#option_container').load("subject.txt");
       alreadyClick = true;
-    } else {
-      container.classList.remove("expandible_class");
-      arrow.classList.add("rotate_down");
-      arrow.classList.remove("rotate_up");
+      $.get("subject.txt",function(){
+        $(".subject_input > input").change(checkSubject);
+      console.log($(".subject_input > input"));
+    });
+    }
+    else {
+      $("#menu")[0].classList.add("rotate_down");
+      $("#menu")[0].classList.remove("rotate_up");
+      $('#option_container').load("subject.txt #empty");
+      selectedSbject.splice[0,-1];
       alreadyClick = false;
     }
+
+    // var container = $("#option_container")[0];
+    // var arrow = $("#menu")[0];
+    // if (!alreadyClick) {
+    //   container.classList.add("expandible_class");
+    //   arrow.classList.remove("rotate_down");
+    //   arrow.classList.add("rotate_up");
+    //   alreadyClick = true;
+    // } else {
+    //   container.classList.remove("expandible_class");
+    //   arrow.classList.add("rotate_down");
+    //   arrow.classList.remove("rotate_up");
+    //   alreadyClick = false;
+    // }
   }
   
+  function checkSubject() {
+    console.log(this.value);
+    if (this.checked == true) {
+      $("#subect_error")[0].innerHTML = "";
+      selectedSbject.push(this.value);
+    } else selectedSbject.splice(this.value.indexOf,1);
+    console.log(selectedSbject);
+  }
+
   function addFunc() {
     //Add and delete row
-    var table = document.getElementById("table_body");
-    tableCount = document.getElementById("table_body").rows.length;
+    var table = $("#table_body")[0];
+    tableCount = $("#table_body")[0].rows.length;
   
     //Check for Empty field
-    var cellLength = document.getElementById("table_body").rows[0].cells.length;
-    var x = document.getElementById("table_body").rows[tableCount - 1];
+    var cellLength = $("#table_body")[0].rows[0].cells.length;
+    var x = $("#table_body")[0].rows[tableCount - 1];
   
     if (!checkEmptyCell(cellLength - 1, x)) {
       console.log("failed");
       return;
     }
-    document.getElementById("message").innerHTML = "";
+    $("#message")[0].innerHTML = "";
   
     var row = table.insertRow(tableCount);
     var cell1 = row.insertCell(0);
@@ -239,16 +263,16 @@
     cell4.innerHTML =
       '<input class="marks" type="number" name="marks" id="marks" value="" placeholder="Marks Obtained">';
     cell5.innerHTML =
-      '<div id="add_and_delete" class="add_and_delete"> <button onclick="addFunc()" type="button" class="add" id="add" name="add" value="+"> + </button> <button onclick="myDeleteFunction()" type="button" class="minus" id="minus" name="minus" value="-"> - </button> </div>';
-  }
+      '<div id="add_and_delete" class="add_and_delete"> <button type="button" onclick="addFunc()" class="add" id="add" name="add" value="+"> + </button> <button type="button" onclick="myDeleteFunction()" class="minus" id="minus" name="minus" value="-"> - </button> </div>';
   
+    }
+    
   function myDeleteFunction() {
-    document.getElementById("table_body").deleteRow(tableCount);
+    $("#table_body")[0].deleteRow(tableCount);
     tableCount--;
   }
-  
+
   function checkEmptyCell(length, row) {
-    document.getElementById("validityMessage").innerHTML = "";
   
     for (var i = 0; i < length; i++) {
       var childList = row.cells[i].childNodes;
@@ -261,7 +285,7 @@
         childList[index].classList.add("redBorder");
         emptyInput.select();
         emptyInput.scrollIntoView();
-        document.getElementById("message").innerHTML =
+        $("#message")[0].innerHTML =
           childList[index].placeholder + " is Incomplete";
         return false;
       }
@@ -269,52 +293,53 @@
         if (childList[index].id == "year") {
           if (!/^[0-9]{4}$/.test(childList[index].value)) {
             emptyInput = childList[index];
-            document.getElementById("message").innerHTML = "Enter Valid Year";
+            $("#message")[0].innerHTML = "Enter Valid Year";
             return false;
           }
         }
         if (childList[index].id == "marks") {
           if (!/^[0-9]*$/.test(childList[index].value)) {
             emptyInput = childList[index];
-            document.getElementById("message").innerHTML = "Enter Valid marks";
+            $("#message")[0].innerHTML = "Enter Valid marks";
             return false;
           }
         }
       }
     }
-    document.getElementById("message").innerHTML = "";
+    $("#message")[0].innerHTML = "";
     return true;
   }
   
-  document.addEventListener('mousedown', removeBorder);
+  $(document).mousedown(removeBorder);
   
   function removeBorder() {
     if (emptyInput != undefined) {
       emptyInput.classList.remove("redBorder");
     }
-    document.getElementById("pwd_message").innerHTML = "";
-    document.getElementById("message").innerHTML = "";
-    document.getElementById("subect_error").innerHTML = "";
-    document.getElementById("hobbies_error").innerHTML = "";
-    document.getElementById("image_error").innerHTML = "";
-    document.getElementById("date_error").innerHTML = "";
+    $("#pwd_message")[0].innerHTML = "";
+    $("#message")[0].innerHTML = "";
+    $("#error")[0].innerHTML = "";
+    $("#subect_error")[0].innerHTML = "";
+    $("#hobbies_error")[0].innerHTML = "";
+    $("#image_error")[0].innerHTML = "";
+    $("#date_error")[0].innerHTML = "";
   }
   
-  var myInput = document.getElementById("pwd");
-  var letter = document.getElementById("letter");
-  var capital = document.getElementById("capital");
-  var number = document.getElementById("number");
-  var length = document.getElementById("length");
+  var myInput = $("#pwd")[0];
+  var letter = $("#letter")[0];
+  var capital = $("#capital")[0];
+  var number = $("#number")[0];
+  var length = $("#length")[0];
   
   myInput.onfocus = function () {
-    document.getElementById("validator").style.display = "block";
+    $("#validator").css("display", "block");
   };
   
   myInput.onblur = function () {
-    document.getElementById("validator").style.display = "none";
+    $("#validator").hide();
   };
   
-  myInput.onkeyup = validator;
+  $("#pwd").keyup(validator);
   
   function validator() {
     var lowerCaseLetters = /[a-z]/g;
@@ -354,8 +379,8 @@
     }
   }
   
-  var confirm = document.getElementById("confirm_pwd");
-  confirm.onkeyup = confirmPassword;
+  var confirm = $("#confirm_pwd")[0];
+  $("#confirm_pwd").keyup(confirmPassword);
   function confirmPassword() {
     if (myInput.value.length != 0) {
       if (confirm.value.length <= myInput.value.length) {
@@ -363,33 +388,36 @@
           confirm.value[confirm.value.length - 1] !=
           myInput.value[confirm.value.length - 1]
         )
-          document.getElementById("pwd_message").innerHTML =
+          $("#pwd_message")[0].innerHTML =
             "confirm password is wrong";
         else {
-          document.getElementById("pwd_message").innerHTML = "";
+          $("#pwd_message")[0].innerHTML = "";
           return true;
         }
       } else
-        document.getElementById("pwd_message").innerHTML =
+      $("#pwd_message")[0].innerHTML =
           "confirm password is wrong";
     }
     return false;
   }
   
-  var email = document.getElementById("email");
-  email.onblur = validateEmail;
+
+  $("#email").blur(function(callback){
+    if( $("#email")[0].value != "")
+    validateEmail();
+  });
   
   function validateEmail() {
-    if (email.value !="" && !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)) {
-      document.getElementById("error").innerHTML = "Enter valid Email";
+    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(  $("#email")[0].value)) {
+      $("#error")[0].innerHTML = "Enter valid Email";
       return false;
-    } else document.getElementById("error").innerHTML = "";
+    } else $("#error")[0].innerHTML = "";
     return true;
   }
   
   function fileValidation() {
-    var fileInput = document.getElementById("myFile");
-    var imagePara = document.getElementById("image_error");
+    var fileInput = $("#myFile")[0];
+    var imagePara = $("#image_error")[0];
   
     if (fileInput.value == "") {
       imagePara.innerHTML = "upload image file";
@@ -412,21 +440,20 @@
     return true;
   }
   
-  var date = document.getElementById("date_input");
+  var date = $("#date_input")[0];
   
   function validDate() {
     if (/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/.test(date.value)) {
-      document.getElementById("date_error").innerHTML = "";
+      $("#date_error")[0].innerHTML = "";
       return true;
     } else {
       removeBorder();
       emptyInput = date;
       emptyInput.classList.add("redBorder");
-      document.getElementById("date_error").innerHTML = "Enter Valid date";
+      $("#date_error")[0].innerHTML = "Enter Valid date";
       emptyInput.select();
       emptyInput.scrollIntoView();
     }
     return false;
   }
-// });
 
